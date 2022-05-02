b4cbdfc96325c57882c1a19d713a7bd26dab8f92
MasakatsuTagishi <m.t-10.22@docomo.ne.jp>
2022-05-02 10:40:36 +0900

    firstCommit

--- Practice-Delegate/ViewController.swift
+++ Practice-Delegate/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var label: UILabel!
+
+    private let textModel = TextModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLabel()
+        textModel.delegate = self
     }
 
 
+    @IBAction func pressedButton(_ sender: Any) {
+        textModel.changeText()
+    }
+
+    private func setupLabel() {
+        label.text = textModel.text
+    }
+
+    private func updateLabel() {
+        label.text = textModel.text
+    }
+
 }
+
+extension ViewController: TextModelDelegate {
+    func didChangeText() {
+        updateLabel()
+    }
+}
+
+protocol TextModelDelegate: AnyObject {
+    func didChangeText()
+}
+
+class TextModel {
+    weak var delegate: TextModelDelegate?
+
+    private(set) var text = "ボタンを押してください"
+
+    func changeText() {
+        text = "ありがとう"
+        notify()
+    }
+
+    private func notify() {
+        delegate?.didChangeText()
+    }
+}
+
 
